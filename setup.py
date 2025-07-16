e5d4f5427ad84c1fcb9bc156c9bfa48090e194da
varkenvarken <andersmichel8@gmail.com>
2025-07-16 21:04:47 +0200

    prepare for packaging

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name="gentry",
+    version="0.0.1",
+    description="Generic tree with visitors and nice markdown/mermaid representation",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://varkenvarken.github.io/gentrypy/",
+    author="varkenvarken",
+    author_email="test@example.com",
+    license="GPLv3",
+    packages=["gentry"],
+    python_requires=">=3.13",
+    zip_safe=False,
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python :: 3.13",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+)
