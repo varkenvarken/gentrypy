861ffbda12c945c450e0bbc27d74407d841c790a
Michel Anders <varkenvarken@users.noreply.github.com>
2025-07-17 21:23:59 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="gentry",
-    version="0.0.9",
+    version="0.0.10",
     description="Generic tree with visitors and nice markdown/mermaid representation",
     long_description=long_description,
     long_description_content_type="text/markdown",
