077b98b93608e6bd14bd4abe5f13c36fbdf620be
varkenvarken <andersmichel8@gmail.com>
2025-07-22 15:04:10 +0000

    v0.1.5-beta

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="gentry",
-    version="0.1.4",
+    version="0.1.5",
     description="Generic tree with visitors and nice markdown/mermaid representation",
     long_description=long_description,
     long_description_content_type="text/markdown",
